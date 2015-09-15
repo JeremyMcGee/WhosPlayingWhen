9e3eae1aa9b5511d159f6f10a55f1128dfe051c8
Jeremy McGee <jeremy.mcgee@bassettdata.com>
2015-09-15 22:24:31 +0100

    Work up Admin site slightly - add some initial content, changing template defaults.

--- Admin/Admin/Controllers/HomeController.cs
+++ Admin/Admin/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Admin.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "WhosPlayingWhen is a side project of Jeremy McGee.";
 
             return View();
         }
