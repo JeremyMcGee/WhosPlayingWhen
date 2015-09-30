07e676def8acd687c799784e9f23226df6809e6d
Jeremy McGee <jeremy.mcgee@bassettdata.com>
2015-09-30 08:04:51 +0100

    Work up Scanner logic.

---
+++ Admin/Scanner/PageConfig.cs
@@ -0,0 +1,10 @@
+namespace Scanner
+{
+    public class PageConfig
+    {
+        public string CalendarListUrl
+        {
+            get { return "http://www.wycliffeprepsport.co.uk/CalendarList.asp?Id=978"; }
+        }
+    }
+}
--- Admin/Scanner/Site.cs
+++ Admin/Scanner/Site.cs
@@ -8,5 +8,64 @@ namespace Scanner
 {
     public class Site
     {
+        public void Scan()
+        {
+            var config = new PageConfig();
+
+            var calendarListPage = new CalendarListPage(config.CalendarListUrl);
+            var fixtures = new List<Fixture>(calendarListPage.GetFixtures());
+
+            foreach (var fixture in fixtures)
+            {
+                foreach (var child in GetChildren())
+                {
+                    var playingStatus = new PlayingStatus(fixture, child);
+                    child.RecordPlayingStatus(playingStatus);
+                    child.InformParentOfPlayingStatus();
+                }
+            }
+        }
+
+        public IEnumerable<Child> GetChildren()
+        {
+            yield break;
+        }
+    }
+
+    public class PlayingStatus
+    {
+        public PlayingStatus(Fixture fixture, Child child)
+        {
+        }
+    }
+
+    public class Child
+    {
+        public void RecordPlayingStatus(PlayingStatus playingStatus)
+        {
+        }
+
+        public void InformParentOfPlayingStatus()
+        {
+        }
+    }
+
+    public class Fixture
+    {
+    }
+
+    public class CalendarListPage
+    {
+        private string url;
+
+        public CalendarListPage(string url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<Fixture> GetFixtures()
+        {
+            yield break;
+        }
     }
 }
